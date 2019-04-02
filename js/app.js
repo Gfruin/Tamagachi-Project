9d55ff9ed3e405efe887060222c61806ae7fff3e
Greg Fruin <gfruin@gmail.com>
2019-04-02 10:13:51 -0500

    beautified and moved event listeners

--- js/app.js
+++ js/app.js
@@ -1,44 +1,44 @@
 
 class Barbarian {
-	constructor(name) {
-		this.name = name;
-		this.hunger = Math.random() * (10 - 1) + 1;
-		this.exhaustion = Math.random() * (10 - 1) + 1;
-		this.rage = 2;
-		this.age = Math.random() * (100 - 1) + 1;
+    constructor(name) {
+        this.name = name;
+        this.hunger = Math.floor(Math.random() * (10 - 1) + 1);
+        this.exhaustion = Math.floor(Math.random() * (10 - 1) + 1);
+        this.rage = 2;
+        this.age = Math.floor(Math.random() * (100 - 1) + 1);
 
-	}
-	attack() {
-		$('.slime').on('click', (e) => {
-			console.log('clicked!');
-			if(this.hunger >= 0) {
-				this.hunger = this.hunger - 1;
-			}
-			this.rage = this.rage + 1 ;
-			
-			
-		})
-		// this.rage = this.rage + 1;
-		console.log( this.name +  " attacks!!");
-		
-		}
-	}
+    }
+    attack() {
+
+        if (this.hunger > 0) {
+            this.hunger = this.hunger - 1;
+        }
+        this.rage = this.rage + 1;
+
+        // this.rage = this.rage + 1;
+        console.log(this.name + " attacks!!");
+
+    }
+    induceRage() {
+        if (this.rage > 0) {
+            this.rage = this.rage + 1;
+
+        }
+    }
+}
+
 
 const game = {
-	currentPlayer: null,
-	start: function() {
-		this.currentPlayer = new Barbarian('Barb');
-		console.log(this.currentPlayer);
-		this.currentPlayer.attack();
-		
-	}
+    currentPlayer: null,
+    start: function() {
+        this.currentPlayer = new Barbarian('Barb');
+        console.log(this.currentPlayer);
+        this.currentPlayer.attack();
+
+    }
 
 
 }
-
-$('#barbarian').on('click', () => {
-	console.log("RAAAAAAGGGGGGEEEEEEEE!!!!!");
-})
 
 
 
@@ -46,10 +46,19 @@ game.start()
 
 
 
+// for debugging -- gives you console access to the most recently clicked element as $it
+let $it;
+$(document).on('click', (e) => {
+  $it = $(e.target);
+})
 
 
+$('.slime').on('click', (e) => {
+	game.currentPlayer.attack()
+	
+})
 
 
-
-
-
+$('#barbarian').on('click', (e) => {
+			console.log("RAAAAAAGGGGGGEEEEEEEE!!!!!");
+})
